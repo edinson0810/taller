3590b265504ba090f448e32564f78c0f5d719f27
edinson barrios <lache081015@gmail.com>
2024-10-22 13:03:36 -0500

    ejercicio5 finalizado

--- ejercicio5/app.js
+++ ejercicio5/app.js
@@ -1,5 +1,28 @@
-import{calcular}from "./ejercicio5.js";
-let algo = suma(2,3);
-console.log(algo);
+import{calcular} from "./ejercicio5.js";
 
+const Calcular = new calcular();
+
+
+function programa() {
+    let a = parseFloat(prompt("Ingrese el primer numero"));
+    let b = parseFloat(prompt("Ingrese el segundo Numero"));
+     let operacion = prompt("Elige la operacion:suma, resta,multiplicacion, division");
+
+     let resultado;
+
+     if (operacion === 'suma'){
+        resultado = Calcular.suma(a, b);
+     } else if (operacion === 'resta'){
+        resultado = Calcular.resta(a, b);
+     } else if (operacion === 'multiplicacion'){
+        resultado = Calcular.multiplicar(a, b);
+     }else if (opercion === 'division'){
+        resultado = Calcular.division(a, b);
+     }
+alert(`${operacion} = ${resultado}`);
+
+}
+
+
+programa();
 
